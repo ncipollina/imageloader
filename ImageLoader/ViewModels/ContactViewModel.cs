502097c8fb6696e52d1beaf17ded346b24c58fba
Nicholas Cipollina <ncipollina@captechconsulting.com>
2013-08-27 16:07:14 -0400

    Initial commit

---
+++ ImageLoader/ViewModels/ContactViewModel.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+
+namespace ImageLoader.ViewModels
+{
+    public class ContactViewModel : INotifyPropertyChanged
+    {
+        private string _id;
+        /// <summary>
+        /// Sample ViewModel property; this property is used to identify the object.
+        /// </summary>
+        /// <returns></returns>
+        public string ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value != _id)
+                {
+                    _id = value;
+                    NotifyPropertyChanged("ID");
+                }
+            }
+        }
+
+        private string _firstName;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                if (value != _firstName)
+                {
+                    _firstName = value;
+                    NotifyPropertyChanged("FirstName");
+                }
+            }
+        }
+
+        private string _lastName;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                if (value != _lastName)
+                {
+                    _lastName = value;
+                    NotifyPropertyChanged("LastName");
+                }
+            }
+        }
+
+        private string _imageUrl;
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding.
+        /// </summary>
+        /// <returns></returns>
+        public string ImageUrl
+        {
+            get
+            {
+                return _imageUrl;
+            }
+            set
+            {
+                if (value != _imageUrl)
+                {
+                    _imageUrl = value;
+                    NotifyPropertyChanged("ImageUrl");
+                }
+            }
+        }
+
+        private ImageSource _photo;
+
+        public ImageSource Photo
+        {
+            get
+            {
+                return _photo ?? App.DefaultImage();
+            }
+            set
+            {
+                if (_photo == value) return;
+                _photo = value;
+                NotifyPropertyChanged("Photo");
+            }
+        }
+
+        private byte[] _imageData;
+
+        public byte[] ImageData
+        {
+            get { return _imageData; }
+            set
+            {
+                _imageData = value;
+                LoadImageAsync();
+            }
+        }
+
+        private async Task LoadImageAsync()
+        {
+            using (var ms = new MemoryStream(ImageData))
+            {
+                var image = new BitmapImage();
+                image.SetSource(ms);
+                Photo = image;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ ImageLoader/ViewModels/MainViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using ImageLoader.Resources;
+
+namespace ImageLoader.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            this.Contacts = new ObservableCollection<ContactViewModel>();
+        }
+
+        /// <summary>
+        /// A collection for ItemViewModel objects.
+        /// </summary>
+        public ObservableCollection<ContactViewModel> Contacts { get; private set; }
+
+        private string _sampleProperty = "Sample Runtime Property Value";
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding
+        /// </summary>
+        /// <returns></returns>
+        public string SampleProperty
+        {
+            get
+            {
+                return _sampleProperty;
+            }
+            set
+            {
+                if (value != _sampleProperty)
+                {
+                    _sampleProperty = value;
+                    NotifyPropertyChanged("SampleProperty");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sample property that returns a localized string
+        /// </summary>
+        public string LocalizedSampleProperty
+        {
+            get
+            {
+                return AppResources.SampleProperty;
+            }
+        }
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Creates and adds a few ItemViewModel objects into the Contacts collection.
+        /// </summary>
+        public void LoadData()
+        {
+            // Sample data; replace with real data
+            this.Contacts.Add(new ContactViewModel()
+            {
+                ID = "0",
+                FirstName = "Bill",
+                LastName = "Gates",
+                ImageUrl =
+                    "http://www.microsoft.com/global/en-us/news/PublishingImages/bod/billg/gates_web.jpg"
+            });
+            this.Contacts.Add(new ContactViewModel()
+            {
+                ID = "1",
+                FirstName = "Steve",
+                LastName = "Balmer",
+                ImageUrl = "http://1.androidauthority.com/wp-content/uploads/2008/10/ballmer-1.jpg"
+            });
+            this.Contacts.Add(new ContactViewModel()
+            {
+                ID = "2",
+                FirstName = "Paul",
+                LastName = "Allen",
+                ImageUrl = "http://tektype.files.wordpress.com/2010/08/paul_allen2.jpg?w=157&h=224"
+            });
+
+            this.IsDataLoaded = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
